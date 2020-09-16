ffa273a941f23e067d428f69932022cc7002cd78
Hoang Giang Nguyen <hoanggia@uni-bremen.de>
2020-09-17 00:44:12 +0200

    Update

--- src/base_controller.py
+++ src/base_controller.py
@@ -9,6 +9,7 @@ from geometry_msgs.msg import Twist, Pose, Quaternion
 from sensor_msgs.msg import JointState
 
 import tf
+from tf2_ros import TransformException
 
 import math
 
@@ -45,16 +46,20 @@ class BaseControl(object):
     self._as.start()
 
   def joint_states_callback(self, joint_states):
-    
-    self.tf_listener.waitForTransform("map", "base_footprint", rospy.Time(), rospy.Duration(10))
-    t = self.tf_listener.getLatestCommonTime("map", "base_footprint")
-    position, quaternion = self.tf_listener.lookupTransform("map", "base_footprint", t)
+    try:
+      self.tf_listener.waitForTransform("odom_origin", "base_footprint", rospy.Time(), rospy.Duration(10))
+    except TransformException as e:
+      rospy.logwarn("base_contronller couldn't find odom_origin frame")
+      return
+      
+    t = self.tf_listener.getLatestCommonTime("odom_origin", "base_footprint")
+    position, quaternion = self.tf_listener.lookupTransform("odom_origin", "base_footprint", t)
     euler = tf.transformations.euler_from_quaternion(quaternion)
     self.current_positions = [position[0], position[1], euler[2]]
 
     try:
       self.current_velocities = [joint_states.velocity[self.odom_x_joint_index], joint_states.velocity[self.odom_y_joint_index], joint_states.velocity[self.odom_z_joint_index]]
-      # rospy.loginfo("base_controller found odom joint velocites")
+      # rospy.loginfo("base_controller found odom joint velocities")
     except IndexError as e:
       rospy.logwarn("base_controller couldn't find enough odom joint velocities in joint states!")
       return
@@ -64,9 +69,8 @@ class BaseControl(object):
     self.state_pub.publish(state)
 
   def goal_callback(self, goal):
-
     # helper variables
-    rate = rospy.Rate(20)
+    rate = rospy.Rate(20) # TODO: Change hardcode
     success = True
     t = 0
     try:
@@ -86,35 +90,51 @@ class BaseControl(object):
         self._as.set_preempted()
         success = False
         break
-
-      if len(goal.trajectory.points) == t:
-        break
       
-      error_odom_x = abs(self.current_positions[0] - goal.trajectory.points[-1].positions[0])
-      error_odom_y = abs(self.current_positions[1] - goal.trajectory.points[-1].positions[1])
-      error_odom_z = abs(self.current_positions[2] - goal.trajectory.points[-1].positions[2])
-      error = [error_odom_x, error_odom_y, error_odom_z]
+      if t < len(goal.trajectory.points):
+        v_x = goal.trajectory.points[t].velocities[goal_odom_x_joint_index]
+        v_y = goal.trajectory.points[t].velocities[goal_odom_y_joint_index]
+        v_z = goal.trajectory.points[t].velocities[goal_odom_z_joint_index]
+      else:
+        v_x = 0
+        v_y = 0
+        v_z = 0
+
+      error_odom_x_pos = goal.trajectory.points[-1].positions[0] - self.current_positions[0]
+      error_odom_y_pos = goal.trajectory.points[-1].positions[1] - self.current_positions[1]
+      error_odom_z_pos = goal.trajectory.points[-1].positions[2] - self.current_positions[2]
+
+      error_odom_x_vel = goal.trajectory.points[-1].velocities[0] - self.current_velocities[0]
+      error_odom_y_vel = goal.trajectory.points[-1].velocities[1] - self.current_velocities[1]
+      error_odom_z_vel = goal.trajectory.points[-1].velocities[2] - self.current_velocities[2]
+
+      error = [[error_odom_x_pos, error_odom_y_pos, error_odom_z_pos], [error_odom_x_vel, error_odom_y_vel, error_odom_z_vel]]
       print("At " + str(t) + ": error = " + str(error))
 
-      if error_odom_x < 0.01 and error_odom_y < 0.01 and error_odom_z < 0.01 :
+      if abs(error_odom_x_pos) < 0.01 and abs(error_odom_y_pos) < 0.01 and abs(error_odom_z_pos) < 0.01 :
         break
 
       self._feedback.feedback.joint_names = self.joint_names
       self._feedback.feedback.actual.positions = self.current_positions
       self._feedback.feedback.actual.velocities = self.current_velocities
-      self._feedback.feedback.actual.time_from_start.secs += 0.05
+      self._feedback.feedback.actual.time_from_start.secs += 0.05 # TODO: Change hardcode
       
       # publish the feedback
       self._as.publish_feedback(self._feedback.feedback)
 
-      # publish the velocity
+      # Feedback control
+      v_x += 0.5 * error_odom_x_pos + 0.1 * error_odom_x_vel
+      v_y += 0.5 * error_odom_y_pos + 0.1 * error_odom_y_vel
+      v_z += 0.5 * error_odom_z_pos + 0.1 * error_odom_z_vel
+
+      # Transform from map velocities to base velocities
       sin_z = math.sin(self.current_positions[2])
       cos_z = math.cos(self.current_positions[2])
-      v_x = goal.trajectory.points[t].velocities[goal_odom_x_joint_index]
-      v_y = goal.trajectory.points[t].velocities[goal_odom_y_joint_index]
       cmd_vel.linear.x = v_x * cos_z + v_y * sin_z
       cmd_vel.linear.y = -v_x * sin_z + v_y * cos_z
-      cmd_vel.angular.z = goal.trajectory.points[t].velocities[goal_odom_z_joint_index]
+      cmd_vel.angular.z = v_z
+
+      # publish the velocity
       self.cmd_vel_pub.publish(cmd_vel)
       t += 1
       rate.sleep()
