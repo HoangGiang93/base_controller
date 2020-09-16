1cbe720e34d1dc69a10e968b3c07b66a7f256ad7
Hoang Giang Nguyen <hoanggia@uni-bremen.de>
2020-09-16 15:08:22 +0200

    Update

--- src/base_controller.py
+++ src/base_controller.py
@@ -8,6 +8,10 @@ from control_msgs.msg import FollowJointTrajectoryAction, FollowJointTrajectoryA
 from geometry_msgs.msg import Twist, Pose, Quaternion
 from sensor_msgs.msg import JointState
 
+import tf
+
+import math
+
 class BaseControl(object):
   # create messages that are used to publish feedback/result
   _feedback = FollowJointTrajectoryActionFeedback()
@@ -22,12 +26,16 @@ class BaseControl(object):
       self.odom_y_joint_index = joint_states.name.index(odom_y_joint)
       self.odom_z_joint_index = joint_states.name.index(odom_z_joint)
       self.joint_names = [odom_x_joint, odom_y_joint, odom_z_joint]
-      rospy.loginfo("base_control found odom joints")
+      rospy.loginfo("base_controller found odom joints")
     except ValueError as e:
-      rospy.logwarn("base_control couldn't find odom joints in joint states!")
+      rospy.logwarn("base_controller couldn't find odom joints in joint states!")
       return
     
+    # create joint states subscriber
     self.joint_states_sub = rospy.Subscriber('/joint_states', JointState, self.joint_states_callback, queue_size=10)
+
+    # create tf listener
+    self.tf_listener = tf.TransformListener()
 
     # create state publisher
     self.state_pub = rospy.Publisher('{}/state'.format(name_space), JointTrajectoryControllerState, queue_size=10)
@@ -38,18 +46,17 @@ class BaseControl(object):
 
   def joint_states_callback(self, joint_states):
     
-    try:
-      self.current_positions = [joint_states.position[self.odom_x_joint_index], joint_states.position[self.odom_y_joint_index], joint_states.position[self.odom_z_joint_index]]
-      # rospy.loginfo("base_control found odom joint positions")
-    except IndexError as e:
-      rospy.logwarn("base_control couldn't find enough odom joint positions in joint states!")
-      return
+    self.tf_listener.waitForTransform("map", "base_footprint", rospy.Time(), rospy.Duration(10))
+    t = self.tf_listener.getLatestCommonTime("map", "base_footprint")
+    position, quaternion = self.tf_listener.lookupTransform("map", "base_footprint", t)
+    euler = tf.transformations.euler_from_quaternion(quaternion)
+    self.current_positions = [position[0], position[1], euler[2]]
 
     try:
       self.current_velocities = [joint_states.velocity[self.odom_x_joint_index], joint_states.velocity[self.odom_y_joint_index], joint_states.velocity[self.odom_z_joint_index]]
-      # rospy.loginfo("base_control found odom joint velocites")
+      # rospy.loginfo("base_controller found odom joint velocites")
     except IndexError as e:
-      rospy.logwarn("base_control couldn't find enough odom joint velocities in joint states!")
+      rospy.logwarn("base_controller couldn't find enough odom joint velocities in joint states!")
       return
     
     state = JointTrajectoryControllerState()
@@ -67,7 +74,7 @@ class BaseControl(object):
       goal_odom_y_joint_index = goal.trajectory.joint_names.index(odom_y_joint)
       goal_odom_z_joint_index = goal.trajectory.joint_names.index(odom_z_joint)
     except:
-      rospy.loginfo("base_control aborted current goal")
+      rospy.loginfo("base_controller aborted current goal")
       self._as.set_aborted()
       return
 
@@ -83,11 +90,15 @@ class BaseControl(object):
       if len(goal.trajectory.points) == t:
         break
       
-      # error = sum([abs(self.current_positions[i] - goal.trajectory.points[t].positions[i]) for i in range(3)])
-      error = abs(self.current_positions[0] - goal.trajectory.points[t].positions[0])
-      print("At", t, "x_ist = ", self.current_positions[0], "x_soll = ", goal.trajectory.points[t].positions[0])
-      print("At", t, "error = ", error)
-    
+      error_odom_x = abs(self.current_positions[0] - goal.trajectory.points[-1].positions[0])
+      error_odom_y = abs(self.current_positions[1] - goal.trajectory.points[-1].positions[1])
+      error_odom_z = abs(self.current_positions[2] - goal.trajectory.points[-1].positions[2])
+      error = [error_odom_x, error_odom_y, error_odom_z]
+      print("At " + str(t) + ": error = " + str(error))
+
+      if error_odom_x < 0.01 and error_odom_y < 0.01 and error_odom_z < 0.01 :
+        break
+
       self._feedback.feedback.joint_names = self.joint_names
       self._feedback.feedback.actual.positions = self.current_positions
       self._feedback.feedback.actual.velocities = self.current_velocities
@@ -97,8 +108,12 @@ class BaseControl(object):
       self._as.publish_feedback(self._feedback.feedback)
 
       # publish the velocity
-      cmd_vel.linear.x = goal.trajectory.points[t].velocities[goal_odom_x_joint_index]
-      cmd_vel.linear.y = goal.trajectory.points[t].velocities[goal_odom_y_joint_index]
+      sin_z = math.sin(self.current_positions[2])
+      cos_z = math.cos(self.current_positions[2])
+      v_x = goal.trajectory.points[t].velocities[goal_odom_x_joint_index]
+      v_y = goal.trajectory.points[t].velocities[goal_odom_y_joint_index]
+      cmd_vel.linear.x = v_x * cos_z + v_y * sin_z
+      cmd_vel.linear.y = -v_x * sin_z + v_y * cos_z
       cmd_vel.angular.z = goal.trajectory.points[t].velocities[goal_odom_z_joint_index]
       self.cmd_vel_pub.publish(cmd_vel)
       t += 1
@@ -114,14 +129,14 @@ class BaseControl(object):
       self._as.set_succeeded()
 
 if __name__ == '__main__':
-  rospy.init_node("base_control")
+  rospy.init_node("base_controller")
   name_space = rospy.get_param('~name_space')
   odom_x_joint = rospy.get_param('{}/odom_x_joint'.format(name_space))
   odom_y_joint = rospy.get_param('{}/odom_y_joint'.format(name_space))
   odom_z_joint = rospy.get_param('{}/odom_z_joint'.format(name_space))
 
   # publish info to the console for the user
-  rospy.loginfo("base_control starts")
+  rospy.loginfo("base_controller starts")
 
   # start the base control
   BaseControl()
